f4360e62850c499c497b799f79e6ab8d92affef4
Mateus Borges <borgesm.work@gmail.com>
2025-07-16 15:08:34 -0300

    feat: install and set react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Toaster } from './components/ui/sonner';
@@ -5,14 +7,23 @@ import Transactions from './features/transactions';
 
 function App() {
     return (
-        <div className="min-h-screen bg-background flex flex-col">
-            <Toaster />
-            <Header />
-            <main className="flex-1 flex items-top justify-center p-4">
-                <Transactions />
-            </main>
-            <Footer />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div className="min-h-screen bg-background flex flex-col">
+                            <Toaster />
+                            <Header />
+                            <main className="flex-1 flex items-top justify-center p-4">
+                                <Transactions />
+                            </main>
+                            <Footer />
+                        </div>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
